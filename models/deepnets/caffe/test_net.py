370f94a284669b32d8e0eebddc1494db8a42619c
Francisco Canas <mailfrancisco@gmail.com>
2015-07-27 16:26:12 -0400

    Updates combined caffe model

--- models/deepnets/caffe/test_net.py
+++ models/deepnets/caffe/test_net.py
@@ -2,13 +2,24 @@ import sys, os
 import numpy as np
 import tables as tb
 
-net_path = '/projects/francisco/repositories/NI-ML/models/deepnets/caffe/ff/hc/mcicn/right'
+net_path = '/projects/francisco/repositories/NI-ML/models/deepnets/caffe/combined/pretrained/'
+#net_path = '/projects/francisco/repositories/NI-ML/models/deepnets/caffe/ff/hc/adcn/left/'
 caffe_root = '/home/fran/workspace/caffe/'  # this file is expected to be in {caffe_root}/examples
 sys.path.insert(0, caffe_root + 'python')
 os.chdir(net_path)
 
-data_node = 'r_hc_features'
-labels_node = 'label'
+combined = True
+#fused = '_fused'
+fused = ''
+side = 'l'
+
+if combined:
+    data_node_l = 'l_hc_features{}'.format(fused)
+    data_node_r = 'r_hc_features{}'.format(fused)
+else:
+    data_node = '{}_hc_features{}'.format(side, fused)
+
+labels_node = 'label{}'.format(fused)
 
 
 import caffe
@@ -17,25 +28,39 @@ import caffe
 caffe.set_device(0)
 caffe.set_mode_gpu()
 
-fused_data_file = '/projects/francisco/data/caffe/standardized/standardized_input_data_HC_R_mcicn.h5'
-fused_classes_file = '/projects/francisco/data/caffe/standardized/standardized_input_classes_mcicn.h5'
+fused_data_file = '/projects/francisco/data/caffe/standardized/combined/ad_cn_test.h5'
+#fused_classes_file = '/projects/francisco/data/caffe/standardized/standardized_input_classes_adcn.h5'
 
-model_file = './train/_iter_30000.caffemodel'
+model_file = './train/_iter_40000.caffemodel'
 net_file = './net.prototxt'
 
 net = caffe.Net(net_file, model_file, caffe.TEST)
 
-fused_classes = tb.open_file(fused_classes_file, 'r').get_node('/valid_classes_fused')[:]
-fused_data = tb.open_file(fused_data_file, 'r').get_node('/valid_data_fused')[:]
-BATCH = fused_classes.shape[0]
+#fused_classes = tb.open_file(fused_classes_file, 'r').get_node('/test_classes_fused')[:]
+fused_file = tb.open_file(fused_data_file, 'r')
 
-input_features = net.blobs[data_node].data
-net.blobs[data_node].reshape(BATCH, input_features.shape[1])
+fused_classes =  fused_file.get_node('/label_fused')[:]
+BATCH = fused_classes.shape[0]
+if combined:
+    fused_data_l = fused_file.get_node('/l_hc_features_fused')[:]
+    fused_data_r = fused_file.get_node('/r_hc_features_fused')[:]
+    input_features_l = net.blobs[data_node_l].data
+    input_features_r = net.blobs[data_node_r].data
+    net.blobs[data_node_l].reshape(BATCH, input_features_l.shape[1])
+    net.blobs[data_node_r].reshape(BATCH, input_features_r.shape[1])
+    net.blobs[data_node_l].data[...] = fused_data_l
+    net.blobs[data_node_r].data[...] = fused_data_r
+else:
+    fused_data = fused_file.get_node('/{}_hc_features_fused'.format(side))[:]
+    input_features = net.blobs[data_node].data
+    net.blobs[data_node].reshape(BATCH, input_features.shape[1])
+    net.blobs[data_node].data[...] = fused_data
+
 
 input_labels = net.blobs[labels_node].data
 net.blobs[labels_node].reshape(BATCH, 1)
 
-net.blobs[data_node].data[...] = fused_data
+
 net.blobs[labels_node].data[...]=fused_classes.reshape(BATCH,1)
 
 out = net.forward()
