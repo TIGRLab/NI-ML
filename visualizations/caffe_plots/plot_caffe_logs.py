460d858eb78d5c98784b511db3df3d16c0932324
Francisco Canas <mailfrancisco@gmail.com>
2015-07-28 16:03:46 -0400

    Adds error checking to caffe plots

--- visualizations/caffe_plots/plot_caffe_logs.py
+++ visualizations/caffe_plots/plot_caffe_logs.py
@@ -23,17 +23,15 @@ def plot_from_logs(train_file, valid_file):
     :return:
     '''
     tX = np.loadtxt(train_file, skiprows=1, delimiter=',')
-    vX = np.loadtxt(valid_file, skiprows=1, delimiter=',')
-    t_iters = tX[:, 0]
-    v_iters = vX[:, 0]
+    try:
+        t_iters = tX[:, 0]
+    except IndexError:
+        print "Not enough iterations to plot."
+        exit(1)
     seconds = tX[:, 1]
-
-    # Training loss and validation accuracy:
     plt.figure(1)
     plt.subplot(211)
     p1, = plt.plot(t_iters, tX[:, 2], 'b', label="Training Loss")
-    p2, = plt.plot(v_iters, vX[:, 2], 'g', label="Validation Accuracy")
-    p3, = plt.plot(v_iters, vX[:, 3], 'r', label="Validation Loss")
     plt.xlabel('Iterations')
     plt.ylabel('Loss/Accuracy')
     plt.legend(bbox_to_anchor=(0.,1.02, 1., 0.102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
@@ -46,9 +44,15 @@ def plot_from_logs(train_file, valid_file):
     plt.ylabel('Learning Rate')
     plt.legend(loc=3)
     plt.grid()
-
-    #
     plt.title('Caffe Model')
+    # try Validation plots:
+    try:
+        vX = np.loadtxt(valid_file, skiprows=1, delimiter=',')
+        v_iters = vX[:, 0]
+        p2, = plt.plot(v_iters, vX[:, 2], 'g', label="Validation Accuracy")
+        p3, = plt.plot(v_iters, vX[:, 3], 'r', label="Validation Loss")
+    except IndexError:
+        print "Not enough iterations to plot validation."
     plt.show()
 
 
