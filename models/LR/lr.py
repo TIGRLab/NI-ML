5737a3e4242a2edb3748832a4e00e2239f01f44b
Francisco Canas <mailfrancisco@gmail.com>
2015-07-31 17:21:52 -0400

    Adds SVM and LR Spearmint scripts

--- models/LR/lr.py
+++ models/LR/lr.py
@@ -1,1 +1,149 @@
-__author__ = 'francisco'
+import logging
+from docopt import docopt
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.preprocessing import normalize
+from sklearn.svm import LinearSVC, SVC
+from sklearn.tree import DecisionTreeClassifier
+
+import tables as tb
+import numpy as np
+from sklearn import linear_model, decomposition, datasets
+from sklearn.pipeline import Pipeline
+
+# Data Vars:
+source_path = '/projects/francisco/data/caffe/standardized/combined/'
+model = 'PCA Linear Regression'
+
+# Sides to iterate over;
+# ie: sides  = ['l', 'r', 'b']
+sides = ['b']
+structure = 'hc'
+
+# Datasets to iterate over:
+# ie.
+# datasets = ['mci_cn', 'ad_cn']
+datasets = ['mci_cn']
+
+# List of fold filename extensions to iterate over:
+# ie:
+# folds = ['_T1', '_T2', '_T3']
+folds = [''] # No folds.
+
+# Change these when running Spearmint experiments which use only the main and don't iterate over datasets or sides:
+default_side = 'l' # Valid values: l, r, b
+default_dataset = 'mci_cn' # Valid values: mci_cn, ad_cn
+
+
+def pca_lr(n_components, C):
+    logistic = linear_model.LogisticRegression(C=C, verbose=1)
+
+    pca = decomposition.RandomizedPCA(n_components=n_components)
+    pca_lr_classifier = Pipeline(steps=[('pca', pca), ('logistic', logistic)])
+    return pca_lr_classifier
+
+
+def load_dataset(fold, side, dataset):
+    train_data_file = '{}_{}{}.h5'.format(dataset, 'train', fold)
+    valid_data_file = '{}_{}{}.h5'.format(dataset, 'valid', fold)
+    test_data_file = '{}_{}{}.h5'.format(dataset, 'test', fold)
+
+    d = tb.open_file(source_path + train_data_file)
+    d_valid = tb.open_file(source_path + valid_data_file)
+    d_test = tb.open_file(source_path + test_data_file)
+
+
+    if 'b' in side:
+        X_l = d.get_node('/l_{}_features_fused'.format(structure.lower()))[:]
+        X_r = d.get_node('/r_{}_features_fused'.format(structure.lower()))[:]
+        X_vl = d_valid.get_node('/l_{}_features_fused'.format(structure.lower()))[:]
+        X_vr = d_valid.get_node('/r_{}_features_fused'.format(structure.lower()))[:]
+        X_tl = d_test.get_node('/l_{}_features_fused'.format(structure.lower()))[:]
+        X_tr = d_test.get_node('/r_{}_features_fused'.format(structure.lower()))[:]
+
+        X = np.concatenate([X_l, X_r], axis=1)
+        X_v = np.concatenate([X_vl, X_vr], axis=1)
+        X_t = np.concatenate([X_tl, X_tr], axis=1)
+    else:
+        X = d.get_node('/{}_{}_features_fused'.format(side, structure.lower()))[:]
+        X_v = d_valid.get_node('/{}_{}_features_fused'.format(side, structure.lower()))[:]
+        X_t = d_test.get_node('/{}_{}_features_fused'.format(side, structure.lower()))[:]
+
+    X = normalize(X)
+    X_v = normalize(X_v)
+    X_t = normalize(X_t)
+
+    y = d.get_node('/label_fused')[:]
+    y_v = d_valid.get_node('/label_fused')[:]
+    y_t = d_test.get_node('/label_fused')[:]
+
+    d.close()
+    d_valid.close()
+    d_test.close()
+
+    return X, X_v, X_t, y, y_v, y_t
+
+
+def experiment_on_fold(params, X, X_v, X_t, y, y_v, y_t):
+    n_components = params['n_components']
+    C = params['C']
+
+    classifier = pca_lr(n_components, C)
+
+    logging.info('Fitting {}'.format(model))
+    classifier.fit(X, y)
+
+    logging.info('Validating {}'.format(model))
+    vscore = classifier.score(X_v, y_v)
+
+    logging.info('Testing {}'.format(model))
+    tscore = classifier.score(X_t, y_t)
+
+    return vscore, tscore
+
+
+def main(job_id, params, side=default_side, dataset=default_dataset):
+    """
+    Main hook for Spearmint.
+    :param job_id:
+    :param params:
+    :return:
+    """
+    logging.basicConfig(level=logging.INFO)
+    print 'Running Experiment on side {} of dataset {}:'.format(side, dataset)
+    print 'Using Parameters: '
+    print params
+    total_vscore = 0.0
+    total_tscore = 0.0
+
+
+    for i, fold in enumerate(folds):
+        print "Fold {}:".format(i)
+        X, X_v, X_t, y, y_v, y_t = load_dataset(fold, side, dataset)
+        vscore, tscore = experiment_on_fold(params, X, X_v, X_t, y, y_v, y_t)
+        print 'Validation Score: {}'.format(vscore)
+        print 'Test Score: {}'.format(tscore)
+        print
+        total_tscore += tscore
+        total_vscore += vscore
+
+
+    total_tscore /= len(folds)
+    total_vscore /= len(folds)
+
+    print 'Avg Validation Score: {}'.format(total_vscore)
+    print 'Avg Test Score: {}'.format(total_tscore)
+    print
+
+    # Minimize error (for spearmint):
+    return (1.0 - total_vscore)
+
+if __name__ == "__main__":
+    # Entry point when running the script manually. Not run by Spearmint.
+    job_id = 0
+    arguments = {
+        'n_components': 16,
+        'C': 0.5,
+    }
+    for side in sides:
+        for dataset in datasets:
+            main(job_id, arguments, side, dataset)
--- models/clean_spearmint.sh
+++ models/clean_spearmint.sh
@@ -4,9 +4,19 @@ then
    echo "Usage: ./clean_spearmint.sh <path to model>";
    exit 1;
 fi
+CWD=$(pwd)
 
-export XP_PATH=$1
+export XP_PATH=$CWD/$1
 export SPEARMINT_DB_ADDRESS=nissl
 export SPEARMINT_ROOT=/projects/francisco/repositories/Spearmint/spearmint
-echo $XP_PATH
-#$SPEARMINT_ROOT/cleanup.sh $XP_PATH
+cd $SPEARMINT_ROOT
+if [ -d $XP_PATH ]
+then
+  python -c "import spearmint.utils.cleanup as cleanup; import sys; cleanup.cleanup(sys.argv[1])" $XP_PATH
+  cd $XP_PATH
+  echo "Removing old logs from $XP_PATH/output/"
+  rm ./output/*
+  cd $CWD
+else
+  echo "$XP_PATH Not a valid directory."
+fi
